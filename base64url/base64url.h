0ce5c518ff8edb59c7b45ea6f2277ead3eecff77
Mark Spanbroek <mark@spanbroek.net>
2018-04-23 16:57:26 +0200

    Empty project

---
+++ base64url/base64url.h
@@ -0,0 +1,16 @@
+//
+//  base64url.h
+//  base64url
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for base64url.
+FOUNDATION_EXPORT double base64urlVersionNumber;
+
+//! Project version string for base64url.
+FOUNDATION_EXPORT const unsigned char base64urlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <base64url/PublicHeader.h>
+
+
